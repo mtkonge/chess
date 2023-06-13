bd6b587273cbbf3026984a0bedb070728dc6e61b
Mikkel <mtkongsted@gmail.com>
2023-06-13 15:50:27 +0200

    position display on board

--- src/Board.ts
+++ src/Board.ts
@@ -4,8 +4,9 @@ export class Board {
     }
 
     setupBoard() {
-        const boardElement =
-            document.querySelector<HTMLDivElement>("#chess-board")!;
+        const boardElement = document.querySelector<HTMLDivElement>(
+            "#inside-chess-board"
+        )!;
         const gridChars = "abcdefgh";
         for (let i = 8; i > 0; i--) {
             for (let j = 0; j < 8; j++) {
@@ -20,7 +21,37 @@ export class Board {
                 }
             }
         }
+        this.coordinates();
     }
+
+    coordinates() {
+        const chars = "ABCDEFGH";
+        const coords = [
+            document.querySelector<HTMLDivElement>("#top")!,
+            document.querySelector<HTMLDivElement>("#bottom")!,
+            document.querySelector<HTMLDivElement>("#left")!,
+            document.querySelector<HTMLDivElement>("#right")!,
+        ];
+        for (let i = 1; i < 9; i++) {
+            const coordsContent = [
+                document.createElement("h3"),
+                document.createElement("h3"),
+                document.createElement("h3"),
+                document.createElement("h3"),
+            ];
+            for (let j = 0; j < coordsContent.length; j++) {
+                if (j < 2) {
+                    coordsContent[j].className = "coord-value-x";
+                    coordsContent[j].append(chars[i - 1]);
+                } else {
+                    coordsContent[j].className = "coord-value-y";
+                    coordsContent[j].append(i.toString());
+                }
+                coords[j].append(coordsContent[j]);
+            }
+        }
+    }
+
     createTile(color: string, coordinates: string) {
         const tile = document.createElement("div")!;
         tile.className = "tile-" + color;
